1d593e1156efe178fa753648c870ac7fad3b1d74
jawaddle <hjawaddle12@gmail.com>
2024-11-22 18:18:12 -0700

    ESLint and Prettier Setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 const Home = () => {
   return (
-    <h1 className="text-3xl text-white font-black">
+    <h1 className="text-3xl font-black text-white">
       Welcome to DevOverFlow via Next.js
     </h1>
   );
